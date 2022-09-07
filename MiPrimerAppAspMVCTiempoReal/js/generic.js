49d9efccfa9b6b6078e0f67ba2cf9678c2aad5cd
Victor <VictorCarmona9035@hotmail.com>
2022-09-07 19:42:47 +0200

    Agregar archivos de proyecto.

---
+++ MiPrimerAppAspMVCTiempoReal/js/generic.js
@@ -0,0 +1,19 @@
+﻿
+
+function get(id) {
+    //textbox(input type='text') combos(select)
+    return document.getElementById(id).value;
+}
+
+//image (img)
+function getS(id) {
+    return document.getElementById(id).src;
+}
+
+function set(id, valor) {
+    document.getElementById(id).value = valor;
+}
+
+function setS(id, valor) {
+    document.getElementById(id).src = valor;
+}
